8aa81e5a0d572cb123ee8f42658a883ef86da77b
ernestomelchor <nurturelove88@gmail.com>
2020-05-04 19:32:13 -0500

    change message one more time and delete extra comment

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <h1>{{ message }}</h1>
   </div>
 </template>
-// this is a comment i hope, signed dan clair
+
 
 <style>
 </style>
@@ -12,10 +12,10 @@
 export default {
   data: function() {
     return {
-      message: "bye Vue.js",
+      message: "HI AGAIN!"
     };
   },
   created: function() {},
-  methods: {},
+  methods: {}
 };
 </script>
