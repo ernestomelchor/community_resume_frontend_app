debb4b6dc69eefe9ba50bcae2734eb7556fb78ac
Tony <tonyhll1972@gmail.com>
2020-05-05 21:07:18 -0500

    finished the index action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,18 +4,24 @@
     <h1>All Students</h1>
     <div v-for="student in students">
       <h2>{{ student.first_name }} {{ student.last_name }}</h2>
+      <img :src="`${student.photo}`" alt="person's picture" />
+      <br />
+      <br />
+      <h3>{{ student.short_bio }}</h3>
+      <h3>{{ student.experiences[0].job_title}}</h3>
+      <h3>{{ student.experiences[0].start_date}} - {{ student.experiences[0].end_date}}</h3>
+
+      <!-- add button below -->
       <h3>{{ student.email }}</h3>
       <h3>{{ student.phone_number }}</h3>
-      <h3>{{ student.short_bio }}</h3>
       <h3>{{ student.linkedin_url }}</h3>
       <h3>{{ student.resume_url}}</h3>
       <h3>{{ student.github_url }}</h3>
       <h3>{{ student.website_url}}</h3>
 
-      <img :src="`${student.photo}`" alt="person's picture" />
-      <br />
-      <br />
-
+      <div v-for="experience in student.experiences">
+        <h3>{{ experience.job_title }}</h3>
+      </div>
       <a :href="`https://www.twitter.com/${student.twitter_url}`">@{{ student.twitter_url }}</a>
     </div>
   </div>
@@ -32,6 +38,7 @@ export default {
       message: "HI AGAIN!",
       students: [
         {
+          id: 1,
           first_name: "Bob",
           last_name: "Smith",
           email: "bob@example.com",
@@ -43,8 +50,29 @@ export default {
           resume_url: "www.bobsmithresume.com",
           github_url: "www.github.com/bobsmith",
           photo: "bobsmith.jpg",
+          experiences: [
+            {
+              id: 1,
+              start_date: "january 1, 2019",
+              end_date: "Present",
+              job_title: "Web Developer",
+              company_name: "Github",
+              details: "i work on the backend",
+              student_id: 1,
+            },
+            {
+              id: 2,
+              start_date: "january 1, 2019",
+              end_date: "Present",
+              job_title: "Ruby Developer",
+              company_name: "Github",
+              details: "i work on the backend",
+              student_id: 1,
+            },
+          ],
         },
         {
+          id: 2,
           first_name: "Sally",
           last_name: "Jones",
           email: "sally@example.com",
@@ -56,8 +84,20 @@ export default {
           resume_url: "www.sallyjonesresume.com",
           github_url: "www.github.com/sallyjones",
           photo: "sallyjones.jpg",
+          experiences: [
+            {
+              id: 1,
+              start_date: "June1, 2019",
+              end_date: "Present",
+              job_title: "Web Developer",
+              company_name: "Github",
+              details: "i work on the backend",
+              student_id: 2,
+            },
+          ],
         },
         {
+          id: 3,
           first_name: "Freddy",
           last_name: "Hernandez",
           email: "freddy@example.com",
@@ -69,6 +109,17 @@ export default {
           resume_url: "www.freddyhernandezresume.com",
           github_url: "www.github.com/freddyhernandez",
           photo: "freddyhernandez.jpg",
+          experiences: [
+            {
+              id: 1,
+              start_date: "March 1, 2019",
+              end_date: "Present",
+              job_title: "Software Developer",
+              company_name: "Github",
+              details: "i work on the backend",
+              student_id: 3,
+            },
+          ],
         },
       ],
     };
